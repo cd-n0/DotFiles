b8fcfc211029eefc01e0c2e51b04b6995bcf2966
cdn0 <59338209+cd-n0@users.noreply.github.com>
2023-03-16 22:24:42 +0300

    nvim overhaul + java setup thingie

---
+++ .config/nvim/lua/cdn0/remap.lua
@@ -0,0 +1,51 @@
+-- leader key
+vim.keymap.set("n", "<Space>", "<Nop>", { silent = true, remap = false })
+vim.g.mapleader = " "
+-- vim.api.nvim_set_keymap()
+vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
+
+
+-- ThePrimagen's vim bindings he has a great channel check him out
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
+vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
+
+vim.keymap.set("n", "J", "mzJ`z")
+vim.keymap.set("n", "<C-d>", "<C-d>zz")
+vim.keymap.set("n", "<C-u>", "<C-u>zz")
+vim.keymap.set("n", "n", "nzzzv")
+vim.keymap.set("n", "N", "Nzzzv")
+
+-- greatest remap ever
+vim.keymap.set("x", "<leader>p", "\"_dP")
+
+-- next greatest remap ever : asbjornHaland
+vim.keymap.set("n", "<leader>y", "\"+y")
+vim.keymap.set("v", "<leader>y", "\"+y")
+vim.keymap.set("n", "<leader>Y", "\"+Y")
+
+vim.keymap.set("n", "<leader>d", "\"_d")
+vim.keymap.set("v", "<leader>d", "\"_d")
+
+-- This is going to get me cancelled
+-- xd vim.keymap.set("i", "<C-c>", "<Esc>")
+
+vim.keymap.set("n", "Q", "<nop>")
+
+vim.keymap.set("n", "<C-n>", "<cmd>cnext<CR>zz")
+vim.keymap.set("n", "<C-p>", "<cmd>cprev<CR>zz")
+vim.keymap.set("n", "<leader>k", "<cmd>lnext<CR>zz")
+vim.keymap.set("n", "<leader>j", "<cmd>lprev<CR>zz")
+
+vim.keymap.set("n", "<leader>s", ":%s/\\<<C-r><C-w>\\>/<C-r><C-w>/gI<Left><Left><Left>")
+vim.keymap.set("n", "<leader>x", "<cmd>!chmod +x %<CR>", { silent = true })
+vim.g.tex_flavor = 'latex'
+vim.g.vimtex_view_method = 'zathura'
+vim.g.vimtex_quickfix_mode = 0
+vim.opt.conceallevel = 1
+vim.g.tex_conceal = 'abdmg'
+--  Splits open at the bottom and right, which is non-retarded, unlike vim defaults.
+--	set splitbelow splitright
+--  Enable autocompletion:
+--	set wildmode=longest,list,full
+--  Vimtex compile shortcuts
+vim.keymap.set("n", "<leader>c", "<cmd>VimtexCompile<CR>")
